264d1b86825f88a99c621e83b0ae6d37fdfed064
FarhanFardid <farhanfardid009@gmail.com>
2023-01-29 15:53:46 +0600

    final practice of the day

---
+++ JS_practice/money.js
@@ -0,0 +1,16 @@
+var money= 50000;
+if (money >=80000){
+    console.log("I have "+money+ " taka. So i will buy a MACBook."  );
+}
+else if(money>=60000 && money<80000){
+    console.log("I have "+money+ " taka. So i will buy a Gaming Laptop.");
+}
+else if(money>=40000 && money<60000){
+    console.log("I have "+money+ " taka. So i will buy a Lenovo Yoga Laptop.");
+}
+else if(money>20000 && money<40000){
+    console.log("I have "+money+ " taka. So i will buy an used old Laptop.");
+}
+else {
+    console.log("I have "+money+ " taka. I don't have much money to buy a laptop. So i will work with my phone.");
+}
